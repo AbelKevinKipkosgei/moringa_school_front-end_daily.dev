208c1e648b952bc64dddd3c434bc44d971e5044a
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-11-17 09:28:52 +0300

    Added login and signup reducers.

--- client/src/Store.jsx
+++ client/src/Store.jsx
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './slices/userSlice';
+import signUpReducer from './slices/signUpSlice';
+import logInReducer from './slices/logInSlice';
 
 
 const store = configureStore({
   reducer: {
     users: userReducer,
+    signUp: signUpReducer,
+    logIn: logInReducer,
   },
 });
 
