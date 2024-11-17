300420447015a91c760be04f39424a923ef673a6
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-11-17 09:13:44 +0300

    Created the feed page.

---
+++ client/src/pages/FeedPage.jsx
@@ -0,0 +1,13 @@
+import Feed from "../components/Feed";
+
+const FeedPage = () => {
+
+    return (
+        <div>
+            <h1>My feed page</h1>
+            <Feed />
+        </div>
+    )
+}
+
+export default FeedPage;
