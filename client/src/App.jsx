22da198fcbe2af108cd0e60a4a407c2643202d65
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-11-17 09:37:01 +0300

    Removed unnecessary import.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Navbar from "./components/Navbar";
 import Admin from "./pages/Admin";
 import FeedPage from "./pages/FeedPage";
