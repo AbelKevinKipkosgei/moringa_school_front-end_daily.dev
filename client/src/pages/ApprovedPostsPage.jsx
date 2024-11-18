de5318e03870a363283bbdc45cffce1e59aa625c
Abigail Salome <salomeabigail9@gmail.com>
2024-11-18 12:26:59 +0300

    updated ApprovedPostsPage.jsx

---
+++ client/src/pages/ApprovedPostsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ApprovedPosts from '../components/ApprovedPosts'
+
+const ApprovedPostsPage = () => {
+  return (
+    <div>
+        <h2>Approved Posts</h2>
+        <ApprovedPosts />
+    </div>
+  )
+}
+
+export default ApprovedPostsPage
