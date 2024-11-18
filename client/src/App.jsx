f5c3a5af9db6d1fed4ba154e65fe0d44cbe205e9
Abigail Salome <salomeabigail9@gmail.com>
2024-11-18 12:40:06 +0300

    updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,43 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux"; // Import useSelector to access state
 import Navbar from "./components/Navbar";
 import Admin from "./pages/Admin";
 import FeedPage from "./pages/FeedPage";
-import LogInPage from "./pages/LogInPage"
+import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
+import Techwriter from './pages/Techwriter';
+import AdminTechwriterProtectedRoute from "./components/AdminTechwriterProtectedRoute";
+import ManagePosts from "./components/ManagePosts";
+import FlaggedPosts from "./components/FlaggedPosts";
+import ApprovedPosts from "./components/ApprovedPosts";
 
 function App() {
+  // Accessing the state using useSelector
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const userRole = useSelector((state) => state.auth.userRole);
+
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<FeedPage />} />
         <Route path="/admin" element={<Admin />} />
-        <Route path="/login" element={<LogInPage/>} />
+        <Route path="/login" element={<LogInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route
+          path="/techwriter/*"
+          element={
+            <AdminTechwriterProtectedRoute isLoggedIn={isLoggedIn} userRole={userRole}>
+              <Techwriter />
+            </AdminTechwriterProtectedRoute>
+          }
+        >
+          {/* Nested routes inside AdminTechwriterProtectedRoute */}
+          <Route path="manageposts" element={<ManagePosts />} />
+          <Route path="flaggedposts" element={<FlaggedPosts />} />
+          <Route path="approvedposts" element={<ApprovedPosts />} />
+        </Route>
       </Routes>
     </Router>
   );
