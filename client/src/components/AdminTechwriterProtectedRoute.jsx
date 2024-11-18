6f950d65a751f1e3dcd31f7af8951be9c44062f6
Abigail Salome <salomeabigail9@gmail.com>
2024-11-18 12:19:48 +0300

    protected techwriter route

---
+++ client/src/components/AdminTechwriterProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const AdminTechwriterProtectedRoute = ({ children }) => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const userRole = useSelector((state) => state.auth.userRole);
+
+  const allowedRoles = ["admin", "techwriter"];
+
+  if (!isLoggedIn || !allowedRoles.includes(userRole)) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
+
+export default AdminTechwriterProtectedRoute;
