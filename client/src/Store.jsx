fa427226b38ef6199b70fae7c4408190dda277bb
Abigail Salome <salomeabigail9@gmail.com>
2024-11-18 12:40:40 +0300

    updated Store.jsx

--- client/src/Store.jsx
+++ client/src/Store.jsx
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './slices/userSlice';
 import signUpReducer from './slices/signUpSlice';
 import logInReducer from './slices/logInSlice';
+import postsReducer from './slices/postsSlice';
+import authReducer from "./slices/authSlice";
 
 
 const store = configureStore({
@@ -9,6 +11,9 @@ const store = configureStore({
     users: userReducer,
     signUp: signUpReducer,
     logIn: logInReducer,
+    posts: postsReducer,
+    auth: authReducer,
+   
   },
 });
 
