3c88471868afe9c1e4e20ea5237b84e2056def21
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-11-19 13:07:01 +0300

    Configured to conditionally render login, signup and logout.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,42 @@
 import { Link } from "react-router-dom";
-import "../styles/Navbar.css"
+import { useSelector } from "react-redux";
+import Logout from "./Logout"; // Import the Logout component
+import "../styles/Navbar.css";
 
 function Navbar() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn); // Get login status from Redux
+
   return (
     <nav className="navbar">
       <ul className="navbar-list">
-        <li className="navbar-item">
-          <Link to="/Admin">Admin</Link>
-        </li>
+        {isLoggedIn && ( // Show only for logged-in users
+          <li className="navbar-item">
+            <Link to="/admin">Admin</Link>
+          </li>
+        )}
         <li className="navbar-item">
           <Link to="/feed">Feed</Link>
         </li>
-        <li className="navbar-item">
-          <Link to="/login">Login</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/signup">Sign up</Link>
-        </li>
-        <li className="navbar-item">
-            <Link to="/techwriter">Techwriter</Link>
-
-        </li>
+        {!isLoggedIn ? ( // Show Login and Signup if user is not logged in
+          <>
+            <li className="navbar-item">
+              <Link to="/login">Login</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/signup">Sign up</Link>
+            </li>
+          </>
+        ) : (
+          // Show Logout and Techwriter if user is logged in
+          <>
+            <li className="navbar-item">
+              <Link to="/techwriter">Techwriter</Link>
+            </li>
+            <li className="navbar-item">
+              <Logout /> {/* Logout button */}
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
