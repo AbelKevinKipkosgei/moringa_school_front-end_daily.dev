d383b58d889fa7df36e78f47d5231daee5faaf1d
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-11-19 18:17:26 +0300

    Updated manage category.

--- client/src/pages/ManageCategory.jsx
+++ client/src/pages/ManageCategory.jsx
@@ -1,14 +1,14 @@
-import ManageCategory from "../components/ManageCategory"
-import React from "react"
-import "../styles/ManageCategory.css"
+// import ManageCategory from "../components/ManageCategory"
+// import React from "react"
+// import "../styles/ManageCategory.css"
 
 
-const ManageCategory = () => {
-    return (
-        <div>
-            <h2>Manage Category</h2>
-            <ManageCategory />
-        </div>
-    )
-}
-export default ManageCategory
+// const ManageCategory = () => {
+//     return (
+//         <div>
+//             <h2>Manage Category</h2>
+//             <ManageCategory />
+//         </div>
+//     )
+// }
+// export default ManageCategory
