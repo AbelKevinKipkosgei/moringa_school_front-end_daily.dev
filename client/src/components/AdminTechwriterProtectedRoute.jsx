6dde813e1b576d6d768d8400022ddce10d65f13a
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-11-19 13:02:35 +0300

    Component to protect techwriter routes.

--- client/src/components/AdminTechwriterProtectedRoute.jsx
+++ client/src/components/AdminTechwriterProtectedRoute.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
 
-const AdminTechwriterProtectedRoute = ({ children }) => {
-  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const userRole = useSelector((state) => state.auth.userRole);
+const AdminTechwriterProtectedRoute = ({ allowedRoles = [], children }) => {
+  const { isLoggedIn, userRole } = useSelector((state) => state.auth);
+  const location = useLocation(); // Get the current location
 
-  const allowedRoles = ["Admin", "techwriter"];
-
+  // If the user is not logged in or does not have the required role
   if (!isLoggedIn || !allowedRoles.includes(userRole)) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login" state={{ from: location }} replace />;
   }
-  return children;
+
+  return children; // Render the children if authorized
+};
+
+AdminTechwriterProtectedRoute.propTypes = {
+  allowedRoles: PropTypes.arrayOf(PropTypes.string).isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default AdminTechwriterProtectedRoute;
