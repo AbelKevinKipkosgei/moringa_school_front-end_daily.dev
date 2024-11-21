a20799371f3f2ae1877279058b23c70f70f2973d
Wangui <elswangui@gmail.com>
2024-11-21 20:45:15 +0300

    Update src/App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,6 @@ import Techwriter from "./pages/Techwriter";
 import AdminTechwriterProtectedRoute from "./components/AdminTechwriterProtectedRoute";
 import AdminProtectedRoute from "./components/AdminProtectedRoute"
 import ManagePosts from "./components/ManagePosts";
-import FlaggedPosts from "./components/FlaggedPosts";
 import ApprovedPosts from "./components/ApprovedPosts";
 import PostPage from "./pages/PostPage";
 import UnauthorizedPage from "./pages/UnauthorizedPage";
@@ -48,7 +47,7 @@ function App() {
           <Route path="manageusers" element={<ManageUser />} />
           <Route path="managecategory" element={<ManageCategory />} />
           <Route path="approvedposts" element={<ApprovedPosts />} />
-          <Route path="flaggedposts" element={<FlaggedPosts />} />
+          
         </Route>
 
         {/* Techwriter Route */}
@@ -65,7 +64,6 @@ function App() {
           }
         >
           <Route path="manageposts" element={<ManagePosts />} />
-          <Route path="flaggedposts" element={<FlaggedPosts />} />
           <Route path="approvedposts" element={<ApprovedPosts />} />
         </Route>
       </Routes>
