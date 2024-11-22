8ca0a8738c81838b85d9005845792841188e2a6e
Abigail Salome <salomeabigail9@gmail.com>
2024-11-22 12:16:46 +0300

    updated AdminTechwriterProtectedRoute.jsx

--- client/src/components/AdminTechwriterProtectedRoute.jsx
+++ client/src/components/AdminTechwriterProtectedRoute.jsx
@@ -1,20 +1,4 @@
 
-// import React from "react";
-// import { Navigate } from "react-router-dom";
-// import { useSelector } from "react-redux";
-
-// const AdminTechwriterProtectedRoute = ({ children }) => {
-//   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-//   const userRole = useSelector((state) => state.auth.userRole);
-
-//   const allowedRoles = ["Admin", "techwriter"];
-
-//   if (!isLoggedIn || !allowedRoles.includes(userRole)) {
-//     return <Navigate to="/login" />;
-//   }
-//   return children;
-// };
-
 import { Navigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
